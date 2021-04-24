b2be01eb5ec05e642a2106ed630c38551a500cff
tom_glb <tom_glb@test.com>
2021-04-24 21:13:33 +0800

    init repo

---
+++ src/test/java/com/prespringboot/community/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prespringboot.community.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
